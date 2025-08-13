6583281019b2b56ccc7137d8a94bb8728f6d0c6f
Marcel Zieba <marcel.zieba@isovalent.com>
2025-08-13 20:19:06 +0200

    lbipam: do not reallocate IPs on operator restart
    If there was a pool that was filled and had unsatisfied Services, on
    operator restart there was a high chance that we will reshuffle
    assignement of IPs for that pool. This resulted in previously safisfied
    services to either become unsatisfied or get a new IP.
    
    Issue is fixed by not performing any operation on services until full
    sync happens. After that, first we try to reuse IPs for already
    satisfied services and only after that we try to assign additional IPs
    to unsatisfied services.
    
    Additionally, add test that covers this case, simulating restart of
    operator.
    
    Related: #40358
    
    Signed-off-by: Marcel Zieba <marcel.zieba@isovalent.com>
    
    (cherry picked from commit b904b9fc69c4ff1aa8c7b8f2a7010c93ca54d6e8)

--- operator/pkg/lbipam/lbipam_fixture_test.go
+++ operator/pkg/lbipam/lbipam_fixture_test.go
@@ -214,7 +214,7 @@ func (nf *newFixture) UpsertSvc(t *testing.T, svc *slim_core_v1.Service) {
 				t.Fatal(err)
 			}
 		},
-	})
+	}, false)
 }
 
 func (nf *newFixture) DeleteSvc(t *testing.T, svc *slim_core_v1.Service) {
@@ -229,7 +229,7 @@ func (nf *newFixture) DeleteSvc(t *testing.T, svc *slim_core_v1.Service) {
 				t.Fatal(err)
 			}
 		},
-	})
+	}, false)
 }
 
 func (nf *newFixture) GetSvc(namespace, name string) *slim_core_v1.Service {
